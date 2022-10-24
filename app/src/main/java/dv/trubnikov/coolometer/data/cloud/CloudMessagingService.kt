25002539546253e7f82f8bbc930d9309caeb691a
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2022-10-24 17:38:54 +0600

    Change the logic of CloudMessagingService

--- app/src/main/java/dv/trubnikov/coolometer/data/cloud/CloudMessagingService.kt
+++ app/src/main/java/dv/trubnikov/coolometer/data/cloud/CloudMessagingService.kt
@@ -1,13 +1,13 @@
 package dv.trubnikov.coolometer.data.cloud
 
+import androidx.annotation.WorkerThread
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import dv.trubnikov.coolometer.domain.cloud.CloudTokenProvider
 import dv.trubnikov.coolometer.domain.models.CloudMessageParser
+import dv.trubnikov.coolometer.domain.models.Message
 import dv.trubnikov.coolometer.domain.resositories.MessageRepository
-import dv.trubnikov.coolometer.tools.assertFail
-import dv.trubnikov.coolometer.tools.getOr
 import dv.trubnikov.coolometer.tools.onFailure
 import dv.trubnikov.coolometer.ui.widget.WidgetUpdater
 import kotlinx.coroutines.*
@@ -28,12 +28,14 @@ class CloudMessagingService : FirebaseMessagingService() {
     @Inject
     lateinit var widgetUpdater: WidgetUpdater
 
+    @WorkerThread
     override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+        Timber.i("onMessageReceived - ${message.data}")
         handleMessage(message)
     }
 
     override fun onDestroy() {
+        Timber.i("Service is destroying")
         super.onDestroy()
         scope.cancel()
     }
@@ -42,15 +44,45 @@ class CloudMessagingService : FirebaseMessagingService() {
         tokenAnalytics.updateToken(token)
     }
 
-    private fun handleMessage(message: RemoteMessage) {
-        val cloudMessage = CloudMessageParser.parse(message)
-        if (cloudMessage == null) {
-            val error = IllegalArgumentException("Не удалось распарсить сообщение! [$message]")
-            assertFail(error)
-            return
+    private fun handleMessage(remoteMessage: RemoteMessage) {
+        val message = CloudMessageParser.parse(remoteMessage)
+        if (message != null) {
+            scheduleJob(message)
+            handleNow(message)
+        } else {
+            Timber.e("Не удалось распарсить сообщение! [$remoteMessage]")
         }
-        repository.insertMessageBlocking(cloudMessage).onFailure {
-            Timber.e("Не удалось записать сообщение в БД [$cloudMessage]")
+    }
+
+    private fun scheduleJob(message: Message) {
+
+    }
+
+    @WorkerThread
+    private fun handleNow(message: Message) {
+        val job = scope.launch {
+            repository.insertMessage(message).onFailure {
+                Timber.e("Не удалось записать сообщение в БД [$message]")
+            }
         }
+        // We can do it, since it is a worker thread.
+        // On the other, hand if we return to early,
+        // this service will be destroyed.
+        //
+        // So we take as much time as it possible. If we are interrupted,
+        // the job from [scheduleJob] will complete handling instead of us,
+        // but a little bit later.
+        while (!job.isCompleted) {
+            try {
+                Timber.d("Wait for coroutine completion")
+                Thread.sleep(250)
+            } catch (e: InterruptedException) {
+                Timber.d(e, "Worker thread was interrupted")
+                job.cancel(e.message ?: "Thread was interrupted", e)
+                Thread.currentThread().interrupt()
+                return
+            }
+        }
+        Timber.d("Coroutine completed")
     }
 }
