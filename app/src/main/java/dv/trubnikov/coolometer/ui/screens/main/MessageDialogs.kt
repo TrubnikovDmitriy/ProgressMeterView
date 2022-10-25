80bf47536a3481944f06dbfd78270325cd1af053
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2022-10-25 23:48:18 +0600

    Configure colors for dialogs

--- app/src/main/java/dv/trubnikov/coolometer/ui/screens/main/MessageDialogs.kt
+++ app/src/main/java/dv/trubnikov/coolometer/ui/screens/main/MessageDialogs.kt
@@ -1,9 +1,9 @@
 package dv.trubnikov.coolometer.ui.screens.main
 
 import android.app.Activity
+import android.content.Context
 import android.text.SpannableStringBuilder
 import androidx.annotation.StringRes
-import androidx.core.text.bold
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dv.trubnikov.coolometer.R
 import dv.trubnikov.coolometer.domain.models.Message
@@ -12,26 +12,26 @@ import kotlin.math.absoluteValue
 
 fun Activity.showNewMessageDialog(message: Message, onAccept: (Message) -> Unit) {
     val title = buildTitle(R.string.alert_dialog_new_message, message.score)
-    MaterialAlertDialogBuilder(this)
+    AlertDialogBuilder(this)
         .setTitle(title)
         .setMessage(message.text)
         .setPositiveButton(R.string.generic_accept) { _, _ -> onAccept(message) }
-        .setNegativeButton(R.string.generic_close, null)
+        .setOnDismissListener { onAccept(message) }
         .show()
 }
 
 fun Activity.showAcceptMessage(message: Message, onAccept: (Message) -> Unit) {
     val title = buildTitle(R.string.alert_dialog_accept_message, message.score)
-    MaterialAlertDialogBuilder(this)
+    AlertDialogBuilder(this)
         .setTitle(title)
         .setMessage(message.text)
         .setPositiveButton(R.string.generic_accept) { _, _ -> onAccept(message) }
-        .setNegativeButton(R.string.generic_close, null)
+        .setOnDismissListener { onAccept(message) }
         .show()
 }
 
 fun Activity.showPityDialog() {
-    MaterialAlertDialogBuilder(this)
+    AlertDialogBuilder(this)
         .setTitle(R.string.alert_dialog_already_received_title)
         .setMessage(R.string.alert_dialog_already_received_text)
         .setPositiveButton(R.string.generic_okay, null)
@@ -50,7 +50,7 @@ fun Activity.showChoiceDialog(messages: List<Message>, onClickItem: (Message) ->
         }
     }.toTypedArray()
     var checkedIndex = 0
-    MaterialAlertDialogBuilder(this)
+    AlertDialogBuilder(this)
         .setSingleChoiceItems(items, checkedIndex) { _, index -> checkedIndex = index }
         .setTitle(R.string.alert_dialog_choice_title)
         .setPositiveButton(R.string.generic_accept) { _, _ -> onClickItem(messages[checkedIndex]) }
@@ -61,8 +61,13 @@ fun Activity.showChoiceDialog(messages: List<Message>, onClickItem: (Message) ->
 private fun Activity.buildTitle(@StringRes textId: Int, score: Int): SpannableStringBuilder {
     val absScore = score.absoluteValue
     val textScore = score.toStringWithSign()
-    val title = resources.getQuantityString(R.plurals.alert_dialog_received_score, absScore, textScore)
+    val titleScore = resources.getQuantityString(R.plurals.alert_dialog_received_score, absScore, textScore)
     return SpannableStringBuilder()
         .append(getString(textId))
-        .bold { append("\n($title)") }
+        .append(" ($titleScore)")
+}
+
+@Suppress("FunctionName")
+private fun AlertDialogBuilder(context: Context): MaterialAlertDialogBuilder {
+    return MaterialAlertDialogBuilder(context, R.style.Cool_MaterialAlertDialog)
 }
